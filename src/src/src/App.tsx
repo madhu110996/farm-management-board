7523e151afa16b719e5c4726df2c1272508339b9
madhu110996 <madhu.ram32@gmail.com>
2025-02-05 15:23:29 +0530

    Create App.tsx

---
+++ src/src/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import FarmManagementBoard from './farm-management-board'
+
+function App() {
+  return (
+    <div className="App">
+      <FarmManagementBoard />
+    </div>
+  )
+}
+
+export default App
